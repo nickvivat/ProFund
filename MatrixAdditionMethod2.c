b4735b2af1c52124e74c6a42ecb8c0ef65f9fbea
nickvivat <110214026+nickvivat@users.noreply.github.com>
2022-10-02 22:45:24 +0700

    Week-9 Programs

---
+++ MatrixAdditionMethod2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main() {
+
+    int row, column;
+
+    scanf("%d %d", &row, &column);
+
+    if (row < 1 || column > 3) {
+        printf("Invalid input, please try again.");
+    }
+
+    int first_matrix[row][column];
+    int second_matrix[row][column];
+    int result_matrix[row][column];
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            scanf("%d", (*(first_matrix + i) + j));
+        }
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            scanf("%d", (*(second_matrix + i) + j));
+        }
+    }
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            *(*(result_matrix + i) + j) = *(*(first_matrix + i) + j) + *(*(second_matrix + i) + j);
+            printf("%d ", *(*(result_matrix + i) + j));
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+
+}
